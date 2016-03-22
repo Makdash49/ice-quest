3776e458c15dea6e9d31272850a400d28aad6467
Mark McQuillen <mark.mcquillen@river.co>
2016-03-21 19:38:33 -0700

    all red cubes become blue on collision

---
+++ Assets/_Scripts/BoxCollision.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxCollision : MonoBehaviour {
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Collision!!!!!!");
+        //renderer.material.color = new Color(0.5f,1,1);
+        GetComponent<Renderer>().material.color = new Color(0.5f, 1, 1);
+
+    }
+}
